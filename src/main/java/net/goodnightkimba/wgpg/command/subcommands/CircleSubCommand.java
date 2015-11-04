1ede14fd336250e5334a2df9183b7dc648386c0a
Adam Simpson <simpsonadam@ymail.com>
2015-11-04 19:21:36 +1030

    wgpg circle sub command

--- src/main/java/net/goodnightkimba/wgpg/command/subcommands/CircleSubCommand.java
+++ src/main/java/net/goodnightkimba/wgpg/command/subcommands/CircleSubCommand.java
@@ -1,20 +1,42 @@
 package net.goodnightkimba.wgpg.command.subcommands;
 
+import net.goodnightkimba.wgpg.command.CommandInputException;
+import net.goodnightkimba.wgpg.command.UserPermissionException;
 import net.goodnightkimba.wgpg.command.WGPGCommand;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CircleSubCommand extends WGPGCommand {
     public CircleSubCommand() {
         setName("circle");
         addAlias("c");
-        setSyntax("/wgpg circle <radius> <minY> <maxY> [X] [Z] [world]");
-        setArgRange(4, 7);
+        setSyntax("/wgpg circle <regionName> <radius> <minY> <maxY> [X] [Z] [world]");
+        setArgRange(5, 8);
         setPermission("wgpg.generate.circle");
     }
 
     @Override
-	public boolean execute(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean execute(CommandSender sender, Command cmd, String label, String[] args) throws UserPermissionException, CommandInputException {
+        String regionName, radius, minY, maxY, inputX, inputZ, world;
+
+        regionName = args[1];
+        radius = args[2];
+        minY = args[3];
+        maxY = args[4];
+
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            inputX = (args.length >= 6) ? args[5] : String.valueOf(player.getLocation().getX());
+            inputZ = (args.length >= 7) ? args[6] : String.valueOf(player.getLocation().getZ());
+            world = (args.length >= 8) ? args[7] : player.getWorld().getName();
+        } else {
+            inputX = args[5];
+            inputZ = args[6];
+            world = args[7];
+        }
+        validatePolyArgs(regionName, radius, radius, "360", "0", minY, maxY, inputX, inputZ, world, sender);
+        processPolygonArgs(regionName, radius, radius, "360", "0", minY, maxY, inputX, inputZ, world, sender);
         return true;
 	}
 }
