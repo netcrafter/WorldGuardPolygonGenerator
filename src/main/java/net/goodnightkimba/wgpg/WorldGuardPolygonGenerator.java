37db9d3d64a9fede870987454f7f4729a1e200d6
Adam Simpson <simpsonadam@ymail.com>
2015-11-18 21:05:51 +1030

    Added license

--- src/main/java/net/goodnightkimba/wgpg/WorldGuardPolygonGenerator.java
+++ src/main/java/net/goodnightkimba/wgpg/WorldGuardPolygonGenerator.java
@@ -1,6 +1,19 @@
 /*
-    WorldGuard Polygon Generator (WGPG) - A Bukkit plugin to create custom shapes using WorldGuards polygon regions.
-    Copyright (C) 2013 myminecrafter01 (Adam Simpson)
+ * WorldGuard Polygon Generator (WGPG) - A BukkitAPI plugin to create custom shapes using WorldGuards polygonal regions.
+ * Copyright (C) 2013 Adam Simpson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.goodnightkimba.wgpg;
 
